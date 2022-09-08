054605228481b2ef901a7fde268cb19de70fcd20
ujjwal-2706 <ujjwalmehta2002@gmail.com>
2022-09-08 22:31:06 +0530

    q4 plot made

--- Assignment 1/data/q4/q4.py
+++ Assignment 1/data/q4/q4.py
@@ -1,33 +1,93 @@
 import sys
 import numpy as np
 from matplotlib import pyplot as plt
-file = open(sys.argv[1],'r')
+file = open(sys.argv[1] + '/X.dat','r')
 x_data = (np.loadtxt(file,unpack=True,dtype='float32')).T
 file.close()
-file = open(sys.argv[2],'r')
+file = open(sys.argv[1] + '/Y.dat','r')
 y_data = (np.loadtxt(file,unpack=True,dtype='str')).T
 file.close()
 
+mean_x1 = np.mean(x_data[:,0])
+mean_x2 = np.mean(x_data[:,1])
+sigma_x1 = np.std(x_data[:,0])
+sigma_x2 = np.std(x_data[:,1])
+def normalize(x_data,mean_x1,mean_x2,sigma_x1,sigma_x2):
+    x_data_norm = np.ones((len(x_data),2),dtype='float32')
+    x_data_norm[:,0] = (x_data[:,0] - mean_x1) / (sigma_x1)
+    x_data_norm[:,1] = (x_data[:,1] - mean_x2) / (sigma_x2)
+    return x_data_norm
+x_data_norm = normalize(x_data,mean_x1,mean_x2,sigma_x1,sigma_x2)
+
 #x_data is m*2 matrix and y_data is m*1 column vector
 #Lets say Canada means 1 and Alaska means 0
-x_filter_alaska = x_data[y_data == 'Alaska']
-x_filter_canada = x_data[y_data == 'Canada']
+x_alaska_plot = x_data[y_data == 'Alaska']
+x_canada_plot = x_data[y_data == 'Canada']
+x_filter_alaska = x_data_norm[y_data == 'Alaska']
+x_filter_canada = x_data_norm[y_data == 'Canada']
 mu0 = (np.matmul(np.ones((1,len(x_filter_alaska))),x_filter_alaska)) / (len(x_filter_alaska))
 mu1 = (np.matmul(np.ones((1,len(x_filter_canada))),x_filter_canada)) / (len(x_filter_canada))
-print(mu0)
-print(mu1)
+print("mu0 is : ",mu0)
+print("mu1 is : ",mu1)
 cov = (np.matmul((x_filter_alaska-mu0).T,(x_filter_alaska-mu0)) + np.matmul((x_filter_canada-mu1).T,(x_filter_canada-mu1))) / len(y_data)
-print(cov)
-phi = len(x_filter_canada) / (len(x_data))
-print(phi)
+print("cov is : ",cov)
+phi = len(x_filter_canada) / (len(x_data_norm))
+print("phi is : ",phi)
 cov0 = np.matmul((x_filter_alaska-mu0).T,(x_filter_alaska-mu0)) / len(x_filter_alaska)
 cov1 = np.matmul((x_filter_canada-mu1).T,(x_filter_canada-mu1)) / len(x_filter_canada)
-print(cov0)
-print(cov1)
+print("cov0 is : ",cov0)
+print("cov1 is : ",cov1)
 
+cov_inv_mu = np.matmul(np.linalg.pinv(cov),mu0.T)
+def linear_separator(x1):
+    return mean_x2 + ((-1* cov_inv_mu[0])/((cov_inv_mu[1]) * sigma_x1)) * (x1 - mean_x1) * sigma_x2
+
+
+#in order to write the quadratic we use the coefficient and then calculate the expression
+def quadratic_separtor(x0,x1):
+    cov1_inv = np.linalg.pinv(cov1)
+    cov0_inv = np.linalg.pinv(cov0)
+    cov_diff = cov0_inv - cov1_inv
+    cov_mu_diff = 2 *(np.matmul(cov1_inv,mu1.T) - np.matmul(cov0_inv,mu0.T))
+
+    #--------------------------- start from here const written
+    #const evaluated
+    const = np.log(np.linalg.det(cov0) / np.linalg.det(cov1))
+    const -= np.matmul(mu1,np.matmul(cov1_inv,mu1.T))
+    const += np.matmul(mu0,np.matmul(cov0_inv,mu0.T))
+
+    final_const = const + cov_diff[0,0] * x0 * x0 + cov_mu_diff[0] * x0
+    linear_term = cov_mu_diff[1] + (cov_diff[1,0] + cov_diff[0,1]) * x0
+    square_term = cov_diff[1,1]
+    # return (-1*linear_term - np.sqrt(linear_term * linear_term - 4*square_term*final_const))/(2*square_term)
+    return square_term*x1*x1 + linear_term*x1 + final_const
+
+
+#------------------------------------------------------------------
+#code for plotting the curves 
+# x_values = np.linspace(50,200)
+# plt.plot(x_alaska_plot[:,0],x_alaska_plot[:,1],'o', label= 'Alaska')
+# plt.plot(x_canada_plot[:,0],x_canada_plot[:,1],'^',label= 'Canada')
+# plt.plot(x_values,linear_separator(x_values), label = 'Linear Separator')
+# plt.xlabel("x0 value(un-normalized)")
+# plt.ylabel("x1 value(un-normalized)")
+# plt.title(label = "Data Set vs Linear Separator",color='blue')
+# plt.legend()
+# plt.show()
+#------------------------------------------------------------------
+
+#------------------------------------------------------------------
+#code for plotting quadratic separtor
+x0_val = np.linspace(-3,3,1000)
+x1_val = np.linspace(-3,3,1000)
+x0_val,x1_val = np.meshgrid(x0_val,x1_val)
+z = quadratic_separtor(x0_val,x1_val)
+plt.figure()
 plt.plot(x_filter_alaska[:,0],x_filter_alaska[:,1],'o', label= 'Alaska')
 plt.plot(x_filter_canada[:,0],x_filter_canada[:,1],'^',label= 'Canada')
-plt.xlabel("x0 value")
-plt.ylabel("x1 value")
+plt.contour(x0_val,x1_val,z,[0.0])
+plt.xlabel("x0 value(normalized)")
+plt.ylabel("x1 value(normalized)")
+plt.title(label = "Data Set vs Quadratic Separator",color='blue')
 plt.legend()
 plt.show()
