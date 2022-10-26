ad1b90ad37064892882d10b12747088d62be28f4
Ujjwal Mehta <ujjwalmehta2002@gmail.com>
2022-10-26 23:18:46 +0530

    relu done on neural network

--- Assignment 3/Q2/neural.py
+++ Assignment 3/Q2/neural.py
@@ -33,13 +33,12 @@ for i in range(m):
 
 def sigmoid(x):
     return 1.0/(1.0 + np.exp(-x))
+def relu(x):
+    if x >= 0:
+        return x
+    else:
+        return 0
 
-# def print_array(numpy_array):
-#     row,col = np.shape(numpy_array)
-#     for i in range(row):
-#         for j in range(col):
-#             if numpy_array[i,j] != 0:
-#                 print(numpy_array[i,j])
 #function to find o(l) and net(l) given a theta dictionary and x_data
 #we will always have to add a neuron that outputs 1
 #assume theta for each neuron arranged in every row
@@ -51,10 +50,24 @@ def output(x_data,theta,layers):
     for l in range(1,layers+1):
         input = theta[l] @ input
         input = sigmoid(input)
-        # print_array(input)
         layer_output[l] = input
         feat,points = np.shape(input)
         input = np.vstack((np.ones((1,points),dtype=np.double),input))
+    return layer_output #this dictoinary of matrix o(l) in each column for m points
+
+def output_relu(x_data,theta,layers):
+    feat,points = np.shape(x_data)
+    input = np.vstack((np.ones((1,points),dtype=np.double),x_data))
+    layer_output = {0:x_data}
+    for l in range(1,layers):
+        input = theta[l] @ input
+        input = relu(input)
+        layer_output[l] = input
+        feat,points = np.shape(input)
+        input = np.vstack((np.ones((1,points),dtype=np.double),input))
+    input = theta[layers] @ input
+    input = sigmoid(input)
+    layer_output[layers] = input
     return layer_output #this dictoinary of matrix o(l) in each column for m points
 
 #y_data is matrix of one-hot encoding each row represents a output
@@ -71,6 +84,34 @@ def gradients(x_data,theta,layers,y_data):
         J_l = net_gradients[l+1]
         theta_temp = theta[l+1][:,1:] #remove the constants
         net_gradients[l] = (theta_temp.T @ J_l) * (layer_output[l] * (1-layer_output[l]))
+        row,col = np.shape(layer_output[l-1])
+        theta_gradients[l] = net_gradients[l] @ (np.vstack(((np.ones((1,col),dtype=np.double)),layer_output[l-1])).T)
+    return theta_gradients
+
+def derivate_relu_number(x):
+    if x > 0:
+        return 1.0
+    elif x == 0:
+        return 0.5
+    else:
+        return 0
+def derivate_relu(x):
+    apply = np.vectorize(derivate_relu_number)
+    result = apply(x)
+    return result
+
+def gradient_relu(x_data,theta,layers,y_data):
+    layer_output = output(x_data,theta,layers)
+    theta_gradients = {}
+    net_gradients = {}
+    #its storing a matrix with ith column representing ith data point
+    net_gradients[layers] =(y_data.T - layer_output[layers]) * (layer_output[layers] * (layer_output[layers]-1))
+    row,col = np.shape(layer_output[layers-1])
+    theta_gradients[layers] = net_gradients[layers] @ (np.vstack(((np.ones((1,col),dtype=np.double)),layer_output[layers-1])).T)
+    for l in range(layers-1,0,-1):
+        J_l = net_gradients[l+1]
+        theta_temp = theta[l+1][:,1:] #remove the constants
+        net_gradients[l] = (theta_temp.T @ J_l) * (derivate_relu(layer_output[l]))
         row,col = np.shape(layer_output[l-1])
         theta_gradients[l] = net_gradients[l] @ (np.vstack(((np.ones((1,col),dtype=np.double)),layer_output[l-1])).T)
     return theta_gradients
@@ -131,6 +172,43 @@ def gradient_descent(x_train_norm,y_train_bit,eta,layer_list,batch_size):
             break
     return theta
 
+def gradient_descent_relu(x_train_norm,y_train_bit,eta,layer_list,batch_size):
+    row,col = np.shape(y_train_bit)
+    train_whole = np.hstack((x_train_norm,y_train_bit))
+    np.random.shuffle(train_whole)
+    theta = {}
+    layers = len(layer_list)
+    previous_input = features
+    for layer in range(1,layers+1):
+        neurons = layer_list[layer-1]
+        theta_val = np.random.normal(0,0.1,size=(neurons,1+previous_input)) 
+        previous_input = neurons
+        theta[layer] = theta_val
+    batch_num = 0
+    x_val,y_val = batch_slice(train_whole,batch_size,col,batch_num)
+    theta_gradients = gradient_relu(x_val.T,theta,layers,y_val)
+    total_iterations = 0
+    while True:
+        iteration = 0
+        diff_val = 0
+        while iteration < 100:
+            x_val,y_val = batch_slice(train_whole,batch_size,col,batch_num)
+            theta_gradients = gradient_relu(x_val.T,theta,layers,y_val)
+            for i in range(1,layers+1):
+                theta[i] = theta[i] - (eta/batch_size)*(theta_gradients[i])
+                val = (eta/batch_size)*(theta_gradients[i])
+                num_row,num_col = np.shape(val)
+                diff_val += np.max(val)
+            batch_num += 1
+            iteration += 1
+        diff_val /= iteration
+        diff_val /= layers
+        total_iterations += 1
+        print(total_iterations)
+        if  diff_val < 0.00001 or total_iterations == 2000:
+            break
+    return theta
+
 def adaptive_learning(x_train_norm,y_train_bit,eta,layer_list,batch_size):
     row,col = np.shape(y_train_bit)
     train_whole = np.hstack((x_train_norm,y_train_bit))
@@ -173,7 +251,6 @@ def adaptive_learning(x_train_norm,y_train_bit,eta,layer_list,batch_size):
         if  diff_val < 0.00001 or total_iterations == 6000:
             break
     return theta
-theta = gradient_descent(x_train_normalize,y_train_encoding,0.1,[5,5,10],100)
 def accuracy(theta,x_test,y_test,layers):
     outputs = output(x_test.T,theta,layers)
     final_answer = outputs[layers]
@@ -195,6 +272,8 @@ def accuracy_comparator(y_test,y_pred):
             correct +=1
     return 100*correct/len(y_pred)
 
+
+theta = gradient_descent_relu(x_train_normalize,y_train_encoding,0.1,[100,100,10],100)
 # neural_classifier = MLPClassifier(hidden_layer_sizes=(5,),activation='logistic',solver='adam',max_iter=100,random_state=1)
 # neural_classifier.fit(x_train_normalize,y_train)
 # y_scikit_prediction = neural_classifier.predict(x_test_normalize)
@@ -207,6 +286,8 @@ print(f"Time Taken is : {end_time - start_time}")
 #accuracy on 10 coming with 6000 iterations 85.27% on test data and 89.56% on training data in 197 seconds
 #accuracy on 15 coming with 6000 iterations 86.03% on test data and 90.748% on training data in 498 seconds
 #accuracy on 25 coming with 6000 iterations 79.2% on test data and 83.27% on training data in 1000 seconds
+
+#accuracy on 78.92% on test data and 80.10 on train data with 2000 iterations 2240 seconds
 
 def output_predictions(theta,x_test,layers):
     outputs = output(x_test.T,theta,layers)
