b62ae61e622575f9b91fc2ec7a3719d087b6c614
Ujjwal Mehta <ujjwalmehta2002@gmail.com>
2022-10-28 15:42:00 +0530

    dataset 2 done

--- Assignment 3/Q1/dataset2.py
+++ Assignment 3/Q1/dataset2.py
@@ -6,6 +6,9 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import tree
 from scipy import sparse
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
 
 stop_words = set(stopwords.words('english'))
 vectorizer_review = TfidfVectorizer()
@@ -127,15 +130,39 @@ def accuracy(original, prediction):
             correct += 1
     return (100*correct)/(total)
 
+#this function will give best classifer among the pruned alphas
+def get_best_classifier(classifier,x_validation,y_validation,y_validation_prediction,ccp_alphas,x_train,y_train,x_test,y_test):
+    nodes = []
+    depth = []
+    answer = {0: classifier}
+    validation_max = accuracy(y_validation,y_validation_prediction)
+    train_accuracy = []
+    test_accuracy = []
+    validation_accuracy = []
+    for alpha in ccp_alphas:
+        new_classifier = tree.DecisionTreeClassifier(ccp_alpha=alpha)
+        new_classifier.fit(x_train,y_train)
+        prediction_train = new_classifier.predict(x_train)
+        prediction_test = new_classifier.predict(x_test)
+        prediction_validation = new_classifier.predict(x_validation)
+        acc_train = accuracy(y_train,prediction_train)
+        acc_test = accuracy(y_test,prediction_test)
+        acc_validation = accuracy(y_validation,prediction_validation)
+        train_accuracy.append(acc_train)
+        test_accuracy.append(acc_test)
+        validation_accuracy.append(acc_validation)
+        nodes.append(new_classifier.tree_.node_count)
+        depth.append(new_classifier.tree_.max_depth)
+        if acc_validation > validation_max:
+            answer[0] = new_classifier
+    return (nodes,depth,train_accuracy,test_accuracy,validation_accuracy,answer[0])
+
+
 def run_part(part_number):
     if part_number == 'a':
         x_train,y_train = load_proper_data(train_load,True)
         x_test,y_test = load_proper_data(test_load,False)
         x_validation,y_validation = load_proper_data(validation_load,False)
-        print("processing done")
-        print(x_test)
-        print(x_train)
-        print(x_validation)
         classifier = tree.DecisionTreeClassifier()
         classifier.fit(x_train,y_train)
         y_test_prediction = classifier.predict(x_test)
@@ -145,6 +172,72 @@ def run_part(part_number):
         print(f"Test accuracy is : {accuracy(y_test,y_test_prediction)}")
         print(f"Validation accuracy is : {accuracy(y_validation,y_validation_prediction)}")
         output_file(output_path,y_test_prediction,part_number)
+    elif part_number == 'b':
+        x_train,y_train = load_proper_data(train_load,True)
+        x_test,y_test = load_proper_data(test_load,False)
+        x_validation,y_validation = load_proper_data(validation_load,False)
+        params = {'min_samples_leaf': list(range(2, 100)), 'min_samples_split': [2, 3, 4],'max_depth': list(range(2,20))}
+        classifier = GridSearchCV(tree.DecisionTreeClassifier(),params)
+        classifier.fit(x_train,y_train)
+        y_train_prediction = classifier.predict(x_train)
+        y_test_prediction = classifier.predict(x_test)
+        y_validation_prediction = classifier.predict(x_validation)
+        print(f"Train accuracy is : {accuracy(y_train,y_train_prediction)}")
+        print(f"Test accuracy is : {accuracy(y_test,y_test_prediction)}")
+        print(f"Validation accuracy is : {accuracy(y_validation,y_validation_prediction)}")
+        output_file(output_path,y_test_prediction,part_number)
+    elif part_number == 'c':
+        x_train,y_train = load_proper_data(train_load,True)
+        x_test,y_test = load_proper_data(test_load,False)
+        x_validation,y_validation = load_proper_data(validation_load,False)
+        classifier = tree.DecisionTreeClassifier()
+        classifier.fit(x_train,y_train)
+        #our goal is to minimize impurity cost
+        #the more the impurity the more the overfitted the tree is 
+        #choose optimal alpha which maximizes both test and training accuracies
+        path = classifier.cost_complexity_pruning_path(x_train,y_train)
+        ccp_alphas, impurities = path.ccp_alphas, path.impurities
+        y_train_prediction = classifier.predict(x_train)
+        y_test_prediction = classifier.predict(x_test)
+        y_validation_prediction = classifier.predict(x_validation)
+        (nodes,depth,train_accuracy,test_accuracy,validation_accuracy,optimal_classifier) = get_best_classifier(classifier,x_validation,y_validation,y_validation_prediction,ccp_alphas,x_train,y_train,x_test,y_test)
+        predict_train = optimal_classifier.predict(x_train)
+        predict_test = optimal_classifier.predict(x_test)
+        predict_validation =  optimal_classifier.predict(x_validation)
+        print(f"Train accuracy is : {accuracy(y_train,predict_train)}")
+        print(f"Test accuracy is : {accuracy(y_test,predict_test)}")
+        print(f"Validation accuracy is : {accuracy(y_validation,predict_validation)}")
+        output_file(output_path,predict_test,'c')
+    elif part_number == 'd':
+        x_train,y_train = load_proper_data(train_load,True)
+        x_test,y_test = load_proper_data(test_load,False)
+        x_validation,y_validation = load_proper_data(validation_load,False)
+        params = {'n_estimators': list(range(50,450,50)), 'max_features': [0.4,0.5,0.6,0.7],'min_samples_split': list(range(2,10,2))}
+        classifier = GridSearchCV(RandomForestClassifier(oob_score=True),params)
+        classifier.fit(x_train,y_train)
+        y_train_prediction = classifier.predict(x_train)
+        y_test_prediction = classifier.predict(x_test)
+        y_validation_prediction = classifier.predict(x_validation)
+        print(f"Train accuracy is : {accuracy(y_train,y_train_prediction)}")
+        print(f"Test accuracy is : {accuracy(y_test,y_test_prediction)}")
+        print(f"Validation accuracy is : {accuracy(y_validation,y_validation_prediction)}")
+        print(f"Out of Bag accuracy is : {classifier.best_estimator_.oob_score_}")
+        output_file(output_path,y_test_prediction,'d')
+    elif part_number == 'e':
+        x_train,y_train = load_proper_data(train_load,True)
+        x_test,y_test = load_proper_data(test_load,False)
+        x_validation,y_validation = load_proper_data(validation_load,False)
+        params = {'n_estimators' : list(range(50,450,50)),'subsample':[0.4,0.5,0.6,0.7],'max_depth':list(range(40,70,10))}
+        classifier = GridSearchCV(xgb.XGBClassifier(use_label_encoder= False),params)
+        classifier.fit(x_train,y_train)
+        y_train_prediction = classifier.predict(x_train)
+        y_test_prediction = classifier.predict(x_test)
+        y_validation_prediction = classifier.predict(x_validation)
+        print(f"Train accuracy is : {accuracy(y_train,y_train_prediction)}")
+        print(f"Test accuracy is : {accuracy(y_test,y_test_prediction)}")
+        print(f"Validation accuracy is : {accuracy(y_validation,y_validation_prediction)}")
+        output_file(output_path,y_test_prediction,'e')
+
 run_part(question_part)
 end_time = time.time()
 print(f"Time taken: {end_time - start_time}")
